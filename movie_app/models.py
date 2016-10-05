f5192d36efe56b64937cac82e14c07bf89abef82
rallen0150 <rallen0150@gmail.com>
2016-10-05 15:35:34 -0400

    remigrated the original problem

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Rater(models.Model):
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    occupation = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=10)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.CharField(max_length=30)
+    video_release_date = models.CharField(max_length=30)
+    imdb_url = models.CharField(max_length=150)
+    unknown = models.CharField(max_length=50)
+    action_genre = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    children = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    musical = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    sci_fi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+class Rating(models.Model):
+    user_id = models.ForeignKey(Rater)
+    movie_id = models.ForeignKey(Movie)
+    rating = models.IntegerField()
+    time_stamp = models.IntegerField()
